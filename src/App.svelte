f713b9c457be132f05aeea0bf0ce7cec4c0f1ace
Thomas Banks <thomasxbanks@gmail.com>
2020-04-19 11:26:26 +0100

    Adds demo introduction

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,6 @@
 <script>
   export let name;
-  import { onMount } from 'svelte';
   import Header from './Header.svelte'
-  import Gallery from './Gallery.svelte'
-  let galleryImages = [];
-
-  onMount(() => {
-    setTimeout(() => {
-      galleryImages = [
-        'https://picsum.photos/1024/768?random=1',
-        'https://picsum.photos/1024/768?random=2',
-        'https://picsum.photos/1024/768?random=3',
-        'https://picsum.photos/1024/768?random=4',
-        'https://picsum.photos/1024/768?random=5',
-        'https://picsum.photos/1024/768?random=6',
-        'https://picsum.photos/1024/768?random=7',
-        'https://picsum.photos/1024/768?random=8',
-        'https://picsum.photos/1024/768?random=9',
-      ];
-    }, 3000)
-  })
 </script>
 
 <style>
@@ -35,8 +16,7 @@
   }
 </style>
 
-<Header hero={false} {name} />
+<Header hero={true} {name} />
 <main>
   <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis nam totam dolore, eaque, quo veniam nihil delectus atque dicta esse, quibusdam earum ratione omnis. Distinctio voluptatum minus error reiciendis architecto!</p>
-  <Gallery images={galleryImages} />
 </main>
